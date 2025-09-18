8140e1357d38527ecc473ca14ba4febf3a571b5f
Ansh Gupta <ansh.gupta3029@gmail.com>
2025-09-18 15:18:57 +0530

    [Ansh]Fix.SamMarks

---
+++ ProgrammingElements/Level1/SamMarks.java
@@ -0,0 +1,11 @@
+public class SamMarks {
+    public static void main(String[] args) {
+
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int totalSubjects = 3;
+        double average = (maths + physics + chemistry) / (double) totalSubjects;
+        System.out.printf("Sam’s average mark in PCM is %.2f\n", average);
+    }
+}
