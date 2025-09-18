962e42c87a709eff736c06bbc5eda7ebf3af684e
Ansh Gupta <ansh.gupta3029@gmail.com>
2025-09-18 16:23:07 +0530

    [Ansh]-Arrays-L2

---
+++ ArraysPrctice/Level2/DigitAnalyzer.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class DigitAnalyzer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int tempNumber = number;
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (tempNumber != 0) {
+            if (index == maxDigit) {
+                System.out.println("Storing only the first " + maxDigit + " digits.");
+                break;
+            }
+            digits[index] = tempNumber % 10;
+            tempNumber /= 10;
+            index++;
+        }
+
+        int largest = 0;
+        int secondLargest = 0;
+
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        System.out.println("The largest digit is: " + largest);
+        System.out.println("The second largest digit is: " + secondLargest);
+        scanner.close();
+    }
+}
