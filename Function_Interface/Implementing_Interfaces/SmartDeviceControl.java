91d7b10997578e332ef11eb4b4405c96ee7ee499
Ansh-dwg <ansh.gupta3029@gmail.com>
2025-11-05 10:26:49 +0530

    Smart Devices Control Interfaces

---
+++ Function_Interface/Implementing_Interfaces/SmartDeviceControl.java
@@ -0,0 +1,44 @@
+interface Controllable {
+    void turnOn();
+    void turnOff();
+}
+
+// Implementation 1: Light
+class Light implements Controllable {
+    @Override
+    public void turnOn() {
+        System.out.println("Light: Switched ON. Brightness set to 100%.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Light: Switched OFF.");
+    }
+}
+
+// Implementation 2: AC
+class AirConditioner implements Controllable {
+    @Override
+    public void turnOn() {
+        System.out.println("AC: Powered ON. Cooling started at 24°C.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("AC: Powered OFF.");
+    }
+}
+
+// Main Test Class
+public class SmartDeviceControl {
+    public static void main(String[] args) {
+        System.out.println("--- Smart Device Control ---");
+        Controllable light = new Light();
+        Controllable ac = new AirConditioner();
+
+        light.turnOn();
+        ac.turnOn();
+        light.turnOff();
+        ac.turnOff();
+    }
+}
